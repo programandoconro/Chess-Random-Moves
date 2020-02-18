88cd5bccb8119dbf811a8460fc8a5435fe6ce509
sony <sony@localhost.localdomain>
2020-02-18 15:14:54 +0000

    Updates

---
+++ precache-manifest.79942c135a316dfbcc82292dcdb93999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb66b8a36b80496dfd35aaabc7976130",
+    "url": "/Chess-Random-Moves/index.html"
+  },
+  {
+    "revision": "ffb990a9acb7f2c57f20",
+    "url": "/Chess-Random-Moves/static/css/main.f9b24a81.chunk.css"
+  },
+  {
+    "revision": "a9badc8d40cf0c75446d",
+    "url": "/Chess-Random-Moves/static/js/2.d2ca3fb8.chunk.js"
+  },
+  {
+    "revision": "9000621e6b71fa7822e8132f7af302f3",
+    "url": "/Chess-Random-Moves/static/js/2.d2ca3fb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffb990a9acb7f2c57f20",
+    "url": "/Chess-Random-Moves/static/js/main.1c5db1ce.chunk.js"
+  },
+  {
+    "revision": "4b16824bf3ebb25ab42b",
+    "url": "/Chess-Random-Moves/static/js/runtime-main.19fe41ae.js"
+  }
+]);
