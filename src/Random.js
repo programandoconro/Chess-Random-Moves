83c5d6001bfeb250b6bbc3f6d346db555519c84e
programandoconro <programandoconro@gmail.com>
2020-02-23 18:23:07 +0000

    random position generator

--- src/Random.js
+++ src/Random.js
@@ -4,18 +4,26 @@ import myRandomPositions from "./const/RandomPositions";
 
 const Random = () => {
   const [position, handlePosition] = useState([]);
+  const [allPositions, addPosition] = useState([]);
 
   const makeRandomPosition = () => {
     handlePosition(myRandomPositions());
+    allPositions.push(" / " + position);
+    addPosition(allPositions);
   };
 
   return (
     <div>
-      <br />
-      <button onClick={e => makeRandomPosition(e)}>Create</button>
-      <hr />
+      <div>
+        <br />
+        <button onClick={e => makeRandomPosition(e)}>Create</button>
+        <hr />
 
-      <Chess pieces={position} allowMoves={false} />
+        <Chess pieces={position} allowMoves={false} />
+      </div>
+      <div>
+        <li>{allPositions}</li>
+      </div>
     </div>
   );
 };
--- src/const/Board.js
+++ src/const/Board.js
@@ -1,21 +1,16 @@
+const myBoard = () => {
+  const board = [];
 
+  const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-const myBoard = () =>{
-const board = [];
+  for (let i = 0; i < letters.length; i++) {
+    for (let index = 0; index < numbers.length; index++) {
+      board.push(letters[i] + numbers[index]);
+    }
+  }
 
-const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
-
-for (let i = 0; i < letters.length; i++) {
-   for (let index = 0; index < numbers.length; index++) {
-      board.push(letters[i] + numbers[index])
-       
-   }
-    
-}
-
-return(board)
-}
-
+  return board;
+};
 
 export default myBoard;
